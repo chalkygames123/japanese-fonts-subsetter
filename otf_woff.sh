18decc10848efea69daa0c242756326d9873047f
Takuya Fukuju <chalkygames123@icloud.com>
2018-04-01 14:16:08 +0900

    保持する OpenType 機能に vert を追加

--- otf_woff.sh
+++ otf_woff.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-for file in `\find src/ -maxdepth 1 -type f -not -name ".DS_Store"`; do pyftsubset $file --text-file=chars.txt --layout-features='palt' --flavor=woff --output-file=dist/`echo ${file##*/} | sed s/.otf$/.woff/`; done
+for file in `\find src/ -maxdepth 1 -type f -not -name ".DS_Store"`; do pyftsubset $file --text-file=chars.txt --layout-features='palt vert' --flavor=woff --output-file=dist/`echo ${file##*/} | sed s/.otf$/.woff/`; done
--- ttf.sh
+++ ttf.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-for file in `\find src/ -maxdepth 1 -type f -not -name ".DS_Store"`; do pyftsubset $file --text-file=chars.txt --layout-features='kern' --output-file=dist/${file##*/}; done
+for file in `\find src/ -maxdepth 1 -type f -not -name ".DS_Store"`; do pyftsubset $file --text-file=chars.txt --layout-features='kern vert' --output-file=dist/${file##*/}; done
